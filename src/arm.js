03dc9a846819ac54d73e2c9a89e02511360c2ec0
Michal Herko <michal@herko.it>
2026-05-10 19:55:46 +0200

    more flexible arm

--- src/arm.js
+++ src/arm.js
@@ -23,18 +23,17 @@ const centerNegative =
     yy(translateY(-10 - 2, connect({ length: 10, slack: 0.2 }))))
  
 const link = (startWidth, startHeight, endWidth, endHeight, length) => {
-  const startArea = (startWidth + endWidth) / 2 * startHeight
+  const startArea = startWidth * startHeight
   const endArea = endWidth * endHeight
   const segments = []
   const e = 0.5
-  const q = 0.8
+  const q = 0.7
   for (let i = 0; i < length; i += e) {
     const area = startArea + (endArea - startArea) * Math.pow(i, q) / Math.pow(length, q)
-    const width = startWidth + (endWidth - startWidth) * i / length
-    const height = area / (endWidth + width) * 2
-    segments.push(translateY(i, hull(
-      cuboid({ size: [width, e, height], center: [0, e / 2, height/2] }),
-      cuboid({ size: [endWidth, e, e], center: [0, e / 2, e / 2] }))))
+    const r = (startHeight / startWidth) + (endHeight / endWidth - startHeight / startWidth) * i / length
+    const width = Math.sqrt(area / r)
+    const height = area / width
+    segments.push(translateY(i, cuboid({ size: [width, e, height], center: [0, e / 2, height/2] })))
   }
   return hullChain(segments)
 }
@@ -48,8 +47,7 @@ const motor =
     hull(
       fourWayRotate(
         translate([radius, radius],
-          cylinder({ radius: 4.2, height: motorHeight, center: [0, 0, motorHeight/2] }))),
-      cuboid({ size: [8, 4.2, motorHeight], center: [0, -14, motorHeight/2] })),
+          cylinder({ radius: 4.2, height: motorHeight, center: [0, 0, motorHeight/2] })))),
     union(
       fourWayRotate(
         translate([radius, radius], cylinder({ radius: 3.2/2, height: 20 }))),
@@ -57,25 +55,31 @@ const motor =
       cone(motorCenterRadius + motorCenterCorner, motorCenterRadius, motorCenterCorner),
       translateZ(motorHeight - motorCenterCorner,
         cone(motorCenterRadius, motorCenterRadius + motorCenterCorner, motorCenterCorner))))
-const motorNegative = cylinder({ radius: 14, height: 30, center: [0, 0, motorHeight + 15] })
+const motorNegative = fourWayRotate(
+  translate([radius, radius, motorHeight], cylinder({ radius: 4, height: 10, center: [0, 0, 5] })))
 
-const linkLength = 100 - radius - 4
+const linkLength = 100 - radius - 3
 
 const holeWidth = 3.2
 const holeHeight = 2
 const holeZ = 3
+
+const holePositive = hull(
+  cuboid({ size: [8, holeWidth, holeZ + holeHeight], center: [0, 0, (holeZ + holeHeight) / 2] }),
+  cuboid({ size: [4, 8, holeZ + holeHeight + 2], center: [0, 0, (holeZ + holeHeight + 2)/2] }))
 
 const holeNegative =
   union(
     cuboid({ size: [50, holeWidth, holeHeight], center: [0, 0, holeZ + holeHeight/2] }),
     cuboid({ size: [6, holeWidth, holeHeight], center: [0, 0, holeZ + holeHeight/2 - 1] }));
 
-const holeTs = [[0, 30], [0, 60]]
+const holeTs = [[0, 20], [0, 60]]
     
 const arm = subtract(
   union(
     centerPositive,
-    link(4, 25, 8, 5, linkLength),
+    link(4, 25, 14, 5, linkLength),
+    holeTs.map((t) => translate(t, holePositive)),
     translateY(100, motor)),
   union(
     centerNegative,
